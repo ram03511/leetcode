fdb98f4dde01e206502e863e25b6398c5005f283
ram03511 <raviravi35320@gmail.com>
2026-09-07 13:20:08 +0530

    Time: 1 ms (100%), Space: 43.4 MB (63.05%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxPower(String s) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < s.length()-1; i++) {
+            if (s.charAt(i) == s.charAt(i+1)) {
+                count++;
+            }
+            else{
+                count = 0;
+            }
+            if (count > max) {
+                max = count;
+            }
+        }
+        return max+1;
+    }
+}
