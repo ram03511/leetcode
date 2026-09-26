e6921d2dd2986c6f7ca1ae9f7d8d180cd468b7f9
ram03511 <raviravi35320@gmail.com>
2026-09-26 10:18:48 +0530

    Time: 20 ms (68.38%), Space: 47.3 MB (38.57%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -4,20 +4,21 @@ class Solution {
         StringBuilder x = new StringBuilder();
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
+            int index = x.length();
             if(ans.isEmpty()){
                 ans.push(ch);
+                x.append(ch);
             }else{
                 char top = ans.peek();
                 if(ch == top){
                     ans.pop();
+                    x.deleteCharAt(index-1);
                 }else{
                     ans.push(ch);
+                    x.append(ch);
                 }
             }
         }
-        for(char c : ans){
-            x.append(c);
-        }
-        return x.reverse().toString();
+        return x.toString();
     }
 }
